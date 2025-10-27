eb7d30d1fd4df4757e11f3d11c9dad828946c68a
GrujicFilipRS <filipgrujicpersonal@gmail.com>
2025-10-27 22:35:07 +0100

    Initial develop

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Accounting
+{
+    class Entrypoint
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
