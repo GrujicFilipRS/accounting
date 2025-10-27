ae2c4a4094a26a780147e77da8a65419763fb1f0
GrujicFilipRS <filipgrujicpersonal@gmail.com>
2025-10-27 23:45:10 +0100

    added session id

--- User.cs
+++ User.cs
@@ -1,29 +1,64 @@
-using BCrypt.Net;
-
 namespace Accounting
 {
     class User
     {
-        private static int workFactor = 8; // Number of iterations of the BCrypt algorithm
-        private static string HashPassword(string password)
-        {
-            return BCrypt.Net.BCrypt.EnhancedHashPassword(password, workFactor);
-        }
-
         public static List<User> users = new List<User>();
 
+        private static string Hash(string password)
+        {
+            const int WORK_FACTOR = 8; // Number of iterations of the BCrypt algorithm
+            return BCrypt.Net.BCrypt.EnhancedHashPassword(password, WORK_FACTOR);
+        }
+
+        private static bool InvalidParams(string username, string password, string fullName)
+        {
+            const int USERNAME_LOWER_BOUND = 5, USERNAME_UPPER_BOUND = 15;
+            const int PASSWORD_LOWER_BOUND = 5, PASSWORD_UPPER_BOUND = 25;
+            const int FULLNAME_LOWER_BOUND = 10, FULLNAME_UPPER_BOUND = 45;
+            
+            if (string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password))
+                return true;
+
+            if (username.Length < USERNAME_LOWER_BOUND || username.Length > USERNAME_UPPER_BOUND)
+                return true;
+
+            if (password.Length < PASSWORD_LOWER_BOUND || password.Length > PASSWORD_UPPER_BOUND)
+                return true;
+
+            if (fullName.Length < FULLNAME_LOWER_BOUND || fullName.Length > FULLNAME_UPPER_BOUND)
+                return true;
+
+            return false;
+        }
+
+        // Returns negative status if an error is given
+        public static string Register(string username, string password, string fullName)
+        {
+            if (InvalidParams(username, password, fullName))
+                throw new Exception("Invalid registration parameters");
+
+            if (users.Any(user => user.username == username))
+                throw new Exception("User already exists with that username");
+
+            User newUser = new User(username, password, fullName);
+
+            return newUser.sessionId;
+        }
+
         private int id;
+        private string sessionId; // Used for handling actions taken by user
         private string username, hashedPassword;
         public string fullName;
 
         public User(string username, string password, string fullName)
         {
             id = users.Count + 1;
+            sessionId = Hash(id.ToString());
             this.username = username;
             this.fullName = fullName;
 
             // Hashing the password, because it will be stored in the db later
-            hashedPassword = HashPassword(password);
+            hashedPassword = Hash(password);
 
             users.Add(this);
         }
