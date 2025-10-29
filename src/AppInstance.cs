9c2cb7699a3ec11b2c7557f0a504e5b074a18744
GrujicFilipRS <filipgrujicpersonal@gmail.com>
2025-10-29 11:58:46 +0100

    Added UTF-8 encoding

--- src/AppInstance.cs
+++ src/AppInstance.cs
@@ -4,6 +4,8 @@ namespace Accounting
     {
         public static void Run()
         {
+            Setup();
+
             string username = "ficfiric";
             string password = "test123";
             string fullName = "Filip Grujic";
@@ -11,11 +13,20 @@ namespace Accounting
             string sessionId = User.Register(username, password, fullName);
 
             string addr = SavingsAccount.CreateAccount(sessionId);
+
             Account.Deposit(sessionId, addr, 1000.0);
+
             Console.WriteLine(Account.GetBalanceFormatted(sessionId, addr));
             Thread.Sleep(61000);
+
             SavingsAccount.ApplyInterest(sessionId, addr);
+
             Console.WriteLine(Account.GetBalanceFormatted(sessionId, addr));
+        }
+        
+        public static void Setup()
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
         }
     }
 }
