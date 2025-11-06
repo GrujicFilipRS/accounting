9b4f3e5067f64653f76545a0607e962c50ca142a
GrujicFilipRS <filipgrujicpersonal@gmail.com>
2025-11-06 15:19:40 +0100

    Bugfix with savings account

--- src/AppInstance.cs
+++ src/AppInstance.cs
@@ -135,7 +135,7 @@ class Application
                 accountSelected = Console.ReadLine();
 
                 if (accountSelected == "C") checkingAddr = CheckingAccount.CreateAccount(session!);
-                if (accountSelected == "S") checkingAddr = SavingsAccount.CreateAccount(session!);
+                if (accountSelected == "S") savingsAddr = SavingsAccount.CreateAccount(session!);
                 Console.WriteLine($"Unknown account type: {accountSelected}");
                 return true;
 
